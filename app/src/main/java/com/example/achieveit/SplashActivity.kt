49f27fd38e7ce70be13b2196e0f2870cba333ad2
Keval Shetta <shetta20keval@gmail.com>
2023-10-04 09:26:29 -0400

    Adding Splash Screen

---
+++ app/src/main/java/com/example/achieveit/SplashActivity.kt
@@ -0,0 +1,29 @@
+package com.example.achieveit
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var splash: ImageView
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        splash = findViewById(R.id.splash);
+
+        splash.alpha = 0f
+
+        splash.animate().setDuration(1500).alpha(1f).withEndAction {
+
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivities(arrayOf(intent))
+            finish()
+        }
+    }
+}
